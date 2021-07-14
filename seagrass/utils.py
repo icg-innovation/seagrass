f17f7ad6accd6d7cbfb454d113a3298d8889fc3f
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2021-07-14 16:32:10 +0000

    Save and extract training data

---
+++ seagrass/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def save_training_data(filepath, X, y):
+    np.save(
+        filepath,
+        np.hstack([X, y])
+    )
+
+
+def extract_training_data(filepath):
+    data = np.load(filepath)
+    X = data[:, :-1]
+    y = data[:, -1]
+
+    return X, y
