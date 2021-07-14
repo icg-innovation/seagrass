917d6a67a7e096b7baf94405cab1a92ad7809fd1
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2021-07-14 16:44:53 +0000

    Reshape y to be explicitly 1D on extraction

--- seagrass/utils.py
+++ seagrass/utils.py
@@ -11,6 +11,6 @@ def save_training_data(filepath, X, y):
 def extract_training_data(filepath):
     data = np.load(filepath)
     X = data[:, :-1]
-    y = data[:, -1]
+    y = data[:, -1].reshape(-1, 1)
 
     return X, y
