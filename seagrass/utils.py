7cdc661a1cba637c23fef3b9fc43107b973d8038
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2021-07-15 10:30:14 +0000

    Expand save_training_data to save in desired filetype

--- seagrass/utils.py
+++ seagrass/utils.py
@@ -1,11 +1,30 @@
 import numpy as np
+import pandas as pd
 
 
-def save_training_data(filepath, X, y):
-    """Save training data in .npy format.
+def save_training_data(filepath, X, y, type, **kwargs):
+    """Save training data in desired format.
 
     Args:
-        filepath (str): Filepath to save training data. Must end in .npy.
+        filepath (str): Filepath to save training data.
+        X (numpy.ndarray): Training data features.
+        y (numpy.ndarray): Machine learning target values.
+    """
+    if type == 'npy':
+        save_training_data_npy(filepath, X, y)
+
+    if type == 'csv':
+        save_training_data_csv(filepath, X, y, **kwargs)
+
+    if type == 'modulos':
+        save_training_data_modulos(filepath, X, y, **kwargs)
+
+
+def save_training_data_npy(filepath, X, y):
+    """Save training data in npy format.
+
+    Args:
+        filepath (str): Filepath to save training data.
         X (numpy.ndarray): Training data features.
         y (numpy.ndarray): Machine learning target values.
     """
@@ -13,6 +32,31 @@ def save_training_data(filepath, X, y):
         filepath,
         np.hstack([X, y])
     )
+
+
+def save_training_data_csv(filepath, X, y, **kwargs):
+    """Save training data in csv format.
+
+    Args:
+        filepath (str): Filepath to save training data.
+        X (numpy.ndarray): Training data features.
+        y (numpy.ndarray): Machine learning target values.
+    """
+    cols = kwargs.get('columns')
+
+    df = pd.DataFrame(np.hstack([X, y]), columns=cols)
+    df.to_csv(filepath)
+
+
+def save_training_data_modulos(filepath, X, y, **kwargs):
+    """Save training data in format to be passed to modulos.
+
+    Args:
+        filepath (str): Filepath to save training data.
+        X (numpy.ndarray): Training data features.
+        y (numpy.ndarray): Machine learning target values.
+    """
+    pass
 
 
 def extract_training_data(filepath):
