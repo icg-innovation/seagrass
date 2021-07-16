4c6463773c8332d628c60fc7b38dcfc1fc0f37ae
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2021-07-16 15:36:49 +0000

    Docstrings and bugfixes

--- seagrass/raster.py
+++ seagrass/raster.py
@@ -30,7 +30,7 @@ def open_from_json(json_filepath):
         sentinel2_filepath = sentinel2_filepath[0]
 
     if type(sentinel2_filepath) is list:
-        sentinel2_mosaic, bathymetry = return_s2_mosaic_projected_depth(
+        sentinel2_mosaic, bathymetry = open_sentinel2_mosaic_and_matched_depth(
             sentinel2_filepath,
             bathymetry_filepath,
             sentinel2_bands,
@@ -55,10 +55,33 @@ def open_sentinel2_mosaic_and_matched_depth(
     sentinel2_filepath,
     bathymetry_filepath,
     sentinel2_bands=None,
-    sentinel2_scale=None,
+    sentinel2_scale=10000,
     bathymetry_nodata=None,
     bathymetry_nodata_threshold=None
 ):
+    """Opens and returns a Sentinel 2 mosaic and a bathymetry raster with matched
+    projections.
+
+    Args:
+        sentinel2_filepath (str): Filepath to the Sentinel 2 raster
+            file.
+        bathymetry_filepath (str): Filepath to the bathymetry raster
+            file.
+        sentinel2_bands (list, optional): List of integers corresponding to
+            the desired Sentinel 2 bands. WARNING: There is an issue when
+            specifying more than four bands.
+        sentinel2_scale (int, optional): Scale factor to obtain the true
+            Sentinel 2 pixel value. Defaults to 10000.
+        bathymetry_nodata (int, optional): Integer value representing pixels
+            containing no data.
+        bathymetry_nodata_threshold (float, optional): Determines threshold
+            where pixels with values less than bathymetry_nodata_threshold
+            will be set equal to bathymetry_nodata instead.
+
+    Returns:
+        tuple: Tuple of numpy.ndarrays containing a Sentinel 2 mosaic raster
+        and a bathymetry raster.
+    """
     sentinel2_mosaic, sentinel2_transform = create_s2_mosaic(
         sentinel2_filepath,
         bathymetry_filepath,
@@ -83,7 +106,7 @@ def open_and_match_rasters(
     sentinel2_scale=10000
 ):
     """Opens and returns Sentinel 2 and bathymetry rasters with matched
-    projections, crs, resolution, etc.
+    projections.
 
     Args:
         sentinel2_filepath (str): Filepath to the Sentinel 2 raster
@@ -96,8 +119,9 @@ def open_and_match_rasters(
         and a bathymetry raster.
     """
     sentinel2 = rioxarray.open_rasterio(sentinel2_filepath)
+    sentinel2 /= sentinel2_scale
 
     bathymetry = rioxarray.open_rasterio(bathymetry_filepath)
     bathymetry = bathymetry.rio.reproject_match(sentinel2)
 
-    return sentinel2.values/sentinel2_scale, bathymetry.values
+    return sentinel2.values, bathymetry.values
