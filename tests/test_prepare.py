d80c7b0dac6f08b00d31c28ec78e24e7d26c9baf
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2021-08-18 09:46:07 +0000

    Cleanup seagrass.prepare unit tests

--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -8,9 +8,9 @@ from numpy.testing import assert_array_equal
 
 
 class TestPrepare(unittest.TestCase):
-    def test_return_features(self):
 
-        data = np.array(
+    def setUp(self):
+        self.data = np.array(
             (
                 [
                     [1, 2, 3],
@@ -25,6 +25,7 @@ class TestPrepare(unittest.TestCase):
             )
         )
 
+    def test_return_features(self):
         expected_features = np.array(
             (
                 [1, 10],
@@ -39,27 +40,11 @@ class TestPrepare(unittest.TestCase):
             )
         )
 
-        features = prepare.return_features(data)
+        features = prepare.return_features(self.data)
 
         assert_array_equal(features, expected_features)
 
     def test_return_features_bands(self):
-
-        data = np.array(
-            (
-                [
-                    [1, 2, 3],
-                    [4, 5, 6],
-                    [7, 8, 9]
-                ],
-                [
-                    [10, 11, 12],
-                    [13, 14, 15],
-                    [16, 17, 18]
-                ]
-            )
-        )
-
         expected_features = np.array(
                 (
                     [1],
@@ -75,29 +60,12 @@ class TestPrepare(unittest.TestCase):
             )
 
         bands = [0]
-
-        features = prepare.return_features(data, bands)
+        features = prepare.return_features(self.data, bands)
 
         assert_array_equal(features, expected_features)
 
     @patch("seagrass.prepare.gaussian_filter")
     def test_return_features_blurring(self, mock_gaussian_filter):
-
-        data = np.array(
-            (
-                [
-                    [1, 2, 3],
-                    [4, 5, 6],
-                    [7, 8, 9]
-                ],
-                [
-                    [10, 11, 12],
-                    [13, 14, 15],
-                    [16, 17, 18]
-                ]
-            )
-        )
-
         expected_features = np.array(
                 (
                     [1, -1],
@@ -117,29 +85,62 @@ class TestPrepare(unittest.TestCase):
         ]
 
         bands = [0]
-        features = prepare.return_features(data, bands, blurring=True)
+        features = prepare.return_features(self.data, bands, blurring=True)
 
         mock_gaussian_filter.assert_called_once()
         mock_gaussian_filter.return_value.ravel.assert_called_once()
         assert_array_equal(features, expected_features)
 
     def test_create_training_data(self):
-
-        data = np.array(
+        ground_truth = np.array(
             (
-                [
-                    [1, 2, 3],
-                    [4, 5, 6],
-                    [7, 8, 9]
-                ],
-                [
-                    [10, 11, 12],
-                    [13, 14, 15],
-                    [16, 17, 18]
-                ]
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+
             )
         )
 
+        no_data_value = -9999
+
+        expected_X = np.array(
+            (
+                [1, 10],
+                [2, 11],
+                [3, 12],
+                [4, 13],
+                [5, 14],
+                [6, 15],
+                [7, 16],
+                [8, 17],
+                [9, 18]
+
+            )
+        )
+
+        expected_y = np.array(
+            (
+                [1],
+                [2],
+                [3],
+                [4],
+                [5],
+                [6],
+                [7],
+                [8],
+                [9]
+
+            )
+        )
+
+        X, y = prepare.create_training_data(
+            self.data, ground_truth, no_data_value
+        )
+
+        assert_array_equal(X, expected_X)
+        assert_array_equal(y, expected_y)
+
+    def test_create_training_data_nodata(self):
         ground_truth = np.array(
             (
                 [-9999, 2, -9999],
@@ -175,7 +176,9 @@ class TestPrepare(unittest.TestCase):
             )
         )
 
-        X, y = prepare.create_training_data(data, ground_truth, no_data_value)
+        X, y = prepare.create_training_data(
+            self.data, ground_truth, no_data_value
+        )
 
         assert_array_equal(X, expected_X)
         assert_array_equal(y, expected_y)
