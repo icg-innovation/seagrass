7e742aeda262be26ccebe942d212e33a210e5f62
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2021-08-26 14:18:35 +0000

    Write raster.py unit tests & generalise syntax

--- seagrass/raster.py
+++ seagrass/raster.py
@@ -2,8 +2,8 @@ import json
 import rioxarray
 
 from seagrass.mosaic import (
-    create_s2_mosaic,
-    return_s2_mosaic_projected_ground_truth
+    create_raster_mosaic,
+    return_mosaic_projected_ground_truth
 )
 
 
@@ -21,44 +21,44 @@ def open_from_json(json_filepath):
     with open(json_filepath) as f:
         data_dict = json.load(f)
 
-    sentinel2_filepath = data_dict["sentinel2_filepath"]
+    raster_filepath = data_dict["raster_filepath"]
     ground_truth_filepath = data_dict["ground_truth_filepath"]
 
-    sentinel2_bands = data_dict.get("sentinel2_bands")
-    sentinel2_scale = data_dict.get("sentinel2_scale", 10000)
+    raster_bands = data_dict.get("raster_bands")
+    raster_scale = data_dict.get("raster_scale", 1)
     ground_truth_nodata = data_dict.get("ground_truth_nodata")
     ground_truth_nodata_threshold = data_dict.get("ground_truth_nodata_threshold")  # noqa: E501
 
-    if type(sentinel2_filepath) is list and len(sentinel2_filepath) == 1:
-        sentinel2_filepath = sentinel2_filepath[0]
+    if type(raster_filepath) is list and len(raster_filepath) == 1:
+        raster_filepath = raster_filepath[0]
 
-    if type(sentinel2_filepath) is list:
-        sentinel2_mosaic, ground_truth = open_and_match_rasters_mosaic(
-            sentinel2_filepath,
+    if type(raster_filepath) is list:
+        raster_mosaic, ground_truth = open_and_match_rasters_mosaic(
+            raster_filepath,
             ground_truth_filepath,
-            sentinel2_bands,
-            sentinel2_scale,
+            raster_bands,
+            raster_scale,
             ground_truth_nodata,
             ground_truth_nodata_threshold
         )
 
-        return sentinel2_mosaic, ground_truth
+        return raster_mosaic, ground_truth
 
     else:
-        sentinel2_image, ground_truth = open_and_match_rasters(
-            sentinel2_filepath,
+        raster_image, ground_truth = open_and_match_rasters(
+            raster_filepath,
             ground_truth_filepath,
-            sentinel2_scale
+            raster_scale
         )
 
-        return sentinel2_image, ground_truth
+        return raster_image, ground_truth
 
 
 def open_and_match_rasters_mosaic(
-    sentinel2_filepath_list,
+    raster_filepath_list,
     ground_truth_filepath,
-    sentinel2_bands=None,
-    sentinel2_scale=10000,
+    raster_bands=None,
+    raster_scale=1,
     ground_truth_nodata=None,
     ground_truth_nodata_threshold=None
 ):
@@ -66,14 +66,14 @@ def open_and_match_rasters_mosaic(
     projections.
 
     Args:
-        sentinel2_filepath_list (str): List of filepaths pointing towards
+        raster_filepath_list (str): List of filepaths pointing towards
             Sentinel 2 raster files to be merged into a single mosaic.
         ground_truth_filepath (str): Filepath to the ground truth raster
             file.
-        sentinel2_bands (list, optional): List of integers corresponding to
+        raster_bands (list, optional): List of integers corresponding to
             the desired Sentinel 2 bands. WARNING: There is an issue when
             specifying more than four bands.
-        sentinel2_scale (int, optional): Scale factor to obtain the true
+        raster_scale (int, optional): Scale factor to obtain the true
             Sentinel 2 pixel value. Defaults to 10000.
         ground_truth_nodata (int, optional): Integer value representing pixels
             containing no data.
@@ -85,53 +85,53 @@ def open_and_match_rasters_mosaic(
         tuple: Tuple of numpy.ndarrays containing a Sentinel 2 mosaic raster
         and a ground truth raster.
     """
-    sentinel2_mosaic, sentinel2_transform = create_s2_mosaic(
-        sentinel2_filepath_list,
+    raster_mosaic, raster_transform = create_raster_mosaic(
+        raster_filepath_list,
         ground_truth_filepath,
-        sentinel2_bands,
-        sentinel2_scale
+        raster_bands,
+        raster_scale
     )
 
-    ground_truth = return_s2_mosaic_projected_ground_truth(
+    ground_truth = return_mosaic_projected_ground_truth(
         ground_truth_filepath,
-        sentinel2_transform,
-        sentinel2_mosaic.shape,
+        raster_transform,
+        raster_mosaic.shape,
         ground_truth_nodata,
         ground_truth_nodata_threshold
     )
 
-    return sentinel2_mosaic, ground_truth
+    return raster_mosaic, ground_truth
 
 
-def open_sentinel2_image(sentinel2_filepath, sentinel2_scale=10000):
+def open_raster_image(raster_filepath, raster_scale=1):
     """Opens and returns the specified Sentinel 2 image, scaled to the true
     pixel value.
 
     Args:
-        sentinel2_filepath (str): Filepath to the Sentinel 2 raster
+        raster_filepath (str): Filepath to the Sentinel 2 raster
             file.
-        sentinel2_scale (int, optional): Scale factor to obtain the true
+        raster_scale (int, optional): Scale factor to obtain the true
             Sentinel 2 pixel value. Defaults to 10000.
 
     Returns:
         xarray.DataArray: Sentinel 2 raster.
     """
-    sentinel2_raster = rioxarray.open_rasterio(sentinel2_filepath)
-    sentinel2_raster /= sentinel2_scale
+    raster = rioxarray.open_rasterio(raster_filepath)
+    raster = raster / raster_scale
 
-    return sentinel2_raster
+    return raster
 
 
 def open_and_match_rasters(
-    sentinel2_filepath,
+    raster_filepath,
     ground_truth_filepath,
-    sentinel2_scale=10000
+    raster_scale=1
 ):
     """Opens and returns Sentinel 2 and ground truth rasters with matched
     projections.
 
     Args:
-        sentinel2_filepath (str): Filepath to the Sentinel 2 raster
+        raster_filepath (str): Filepath to the Sentinel 2 raster
             file.
         ground_truth_filepath (str): Filepath to the ground truth raster
             file.
@@ -140,9 +140,9 @@ def open_and_match_rasters(
         tuple: Tuple of xarray.DataArrays containing a Sentinel 2 raster
         and a ground truth raster.
     """
-    sentinel2 = open_sentinel2_image(sentinel2_filepath, sentinel2_scale)
+    raster = open_raster_image(raster_filepath, raster_scale)
 
     ground_truth = rioxarray.open_rasterio(ground_truth_filepath)
-    ground_truth = ground_truth.rio.reproject_match(sentinel2)
+    ground_truth = ground_truth.rio.reproject_match(raster)
 
-    return sentinel2, ground_truth
+    return raster, ground_truth
---
+++ tests/test_raster.py
@@ -0,0 +1,233 @@
+import unittest
+from unittest.mock import patch, Mock
+from xarray.testing import assert_equal
+
+from seagrass import raster
+
+import numpy as np
+import xarray as xa
+
+
+class TestRaster(unittest.TestCase):
+
+    @patch("seagrass.raster.open_and_match_rasters")
+    @patch("seagrass.raster.json")
+    @patch("builtins.open")
+    def test_open_from_json(
+        self,
+        mock_open,
+        mock_json,
+        mock_open_and_match_rasters
+    ):
+        test_json_filepath = "path/to/json"
+        mock_json.load.return_value = {
+            "raster_filepath": "path/to/raster",
+            "ground_truth_filepath": "path/to/ground/truth",
+            "raster_bands": [0, 1, 2, 3],
+            "raster_scale": 10000,
+            "ground_truth_nodata": -9999,
+            "ground_truth_nodata_threshold": -1e6
+        }
+
+        mock_open_and_match_rasters.return_value = (Mock(), Mock())
+
+        raster_image, ground_truth = raster.open_from_json(test_json_filepath)
+
+        mock_open.assert_called_once_with(test_json_filepath)
+        mock_open.return_value.__enter__.assert_called_once()
+        mock_json.load.assert_called_once_with(
+            mock_open.return_value.__enter__.return_value
+        )
+        mock_open_and_match_rasters.assert_called_once_with(
+            "path/to/raster",
+            "path/to/ground/truth",
+            10000
+        )
+        mock_open.return_value.__exit__.assert_called_once_with(
+            None, None, None
+        )
+
+    @patch("seagrass.raster.open_and_match_rasters_mosaic")
+    @patch("seagrass.raster.json")
+    @patch("builtins.open")
+    def test_open_from_json_mosaic(
+        self,
+        mock_open,
+        mock_json,
+        mock_open_and_match_rasters_mosaic
+    ):
+        test_json_filepath = "path/to/json"
+        mock_json.load.return_value = {
+            "raster_filepath": ["path/to/raster/1", "path/to/raster/2"],
+            "ground_truth_filepath": "path/to/ground/truth",
+            "raster_bands": [0, 1, 2, 3],
+            "raster_scale": 10000,
+            "ground_truth_nodata": -9999,
+            "ground_truth_nodata_threshold": -1e6
+        }
+
+        mock_open_and_match_rasters_mosaic.return_value = (Mock(), Mock())
+
+        raster_image, ground_truth = raster.open_from_json(test_json_filepath)
+
+        mock_open_and_match_rasters_mosaic.assert_called_once_with(
+            ["path/to/raster/1", "path/to/raster/2"],
+            "path/to/ground/truth",
+            [0, 1, 2, 3],
+            10000,
+            -9999,
+            -1e6
+        )
+
+    @patch("seagrass.raster.open_and_match_rasters")
+    @patch("seagrass.raster.json")
+    @patch("builtins.open")
+    def test_open_from_json_list_one_item(
+        self,
+        mock_open,
+        mock_json,
+        mock_open_and_match_rasters
+    ):
+        test_json_filepath = "path/to/json"
+        mock_json.load.return_value = {
+            "raster_filepath": ["path/to/raster"],
+            "ground_truth_filepath": "path/to/ground/truth",
+            "raster_bands": [0, 1, 2, 3],
+            "raster_scale": 10000,
+            "ground_truth_nodata": -9999,
+            "ground_truth_nodata_threshold": -1e6
+        }
+
+        mock_open_and_match_rasters.return_value = (Mock(), Mock())
+
+        raster_image, ground_truth = raster.open_from_json(test_json_filepath)
+
+        mock_open_and_match_rasters.assert_called_once_with(
+            "path/to/raster",
+            "path/to/ground/truth",
+            10000
+        )
+
+    @patch("seagrass.raster.open_and_match_rasters_mosaic")
+    @patch("seagrass.raster.json")
+    @patch("builtins.open")
+    def test_open_from_json_list_dict_defaults(
+        self,
+        mock_open,
+        mock_json,
+        mock_open_and_match_rasters_mosaic
+    ):
+        test_json_filepath = "path/to/json"
+        mock_json.load.return_value = {
+            "raster_filepath": ["path/to/raster/1", "path/to/raster/2"],
+            "ground_truth_filepath": "path/to/ground/truth",
+        }
+
+        mock_open_and_match_rasters_mosaic.return_value = (Mock(), Mock())
+
+        raster_image, ground_truth = raster.open_from_json(test_json_filepath)
+
+        mock_open_and_match_rasters_mosaic.assert_called_once_with(
+            ["path/to/raster/1", "path/to/raster/2"],
+            "path/to/ground/truth",
+            None,
+            1,
+            None,
+            None
+        )
+
+    @patch("seagrass.raster.return_mosaic_projected_ground_truth")
+    @patch("seagrass.raster.create_raster_mosaic")
+    def test_open_and_match_rasters_mosaic(
+        self,
+        mock_create_raster_mosaic,
+        mock_return_mosaic_projected_ground_truth
+    ):
+        test_raster_filepath_list = ["path/to/raster/1", "path/to/raster/2"]
+        test_ground_truth_filepath = "path/to/ground/truth"
+        test_raster_bands = [0, 1, 2, 3]
+        test_raster_scale = 10000
+        test_ground_truth_nodata = -9999
+        test_ground_truth_nodata_threshold = -1e6
+
+        mock_raster_mosaic = Mock()
+        mock_raster_transform = Mock()
+        mock_create_raster_mosaic.return_value = (
+            mock_raster_mosaic, mock_raster_transform
+        )
+        mock_return_mosaic_projected_ground_truth.return_value = Mock()
+
+        raster.open_and_match_rasters_mosaic(
+            test_raster_filepath_list,
+            test_ground_truth_filepath,
+            test_raster_bands,
+            test_raster_scale,
+            test_ground_truth_nodata,
+            test_ground_truth_nodata_threshold
+        )
+
+        mock_create_raster_mosaic.assert_called_once_with(
+            test_raster_filepath_list,
+            test_ground_truth_filepath,
+            test_raster_bands,
+            test_raster_scale
+        )
+
+        mock_return_mosaic_projected_ground_truth.assert_called_once_with(
+            test_ground_truth_filepath,
+            mock_raster_transform,
+            mock_raster_mosaic.shape,
+            test_ground_truth_nodata,
+            test_ground_truth_nodata_threshold
+        )
+
+    @patch("seagrass.raster.rioxarray")
+    def test_open_raster_image(self, mock_rioxarray):
+        test_filepath = "path/to/raster"
+        test_scale = 10000
+        test_data = np.array(
+            (
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            )
+        )
+
+        expected_raster = xa.DataArray(test_data)
+        mock_rioxarray.open_rasterio.return_value = expected_raster
+
+        out_raster = raster.open_raster_image(test_filepath, test_scale)
+
+        mock_rioxarray.open_rasterio.assert_called_once_with(test_filepath)
+        assert_equal(out_raster, expected_raster/10000)
+
+    @patch("seagrass.raster.rioxarray")
+    @patch("seagrass.raster.open_raster_image")
+    def test_open_and_match_rasters(
+        self,
+        mock_open_raster_image,
+        mock_rioxarray
+    ):
+        test_raster_filepath = "path/to/raster"
+        test_ground_truth_filepath = "path/to/ground/truth"
+        test_raster_scale = 10000
+
+        mock_raster = mock_open_raster_image.return_value
+        mock_ground_truth = mock_rioxarray.open_rasterio.return_value
+
+        raster.open_and_match_rasters(
+            test_raster_filepath,
+            test_ground_truth_filepath,
+            test_raster_scale
+        )
+
+        mock_open_raster_image.assert_called_once_with(
+            test_raster_filepath,
+            test_raster_scale
+        )
+        mock_rioxarray.open_rasterio.assert_called_once_with(
+            test_ground_truth_filepath
+        )
+        mock_ground_truth.rio.reproject_match.assert_called_once_with(
+            mock_raster
+        )
